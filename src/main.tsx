a4954d95380f85b2cffcd5d3c987f34036766b3d
thoriqdharmawan <thoriqd21@gmail.com>
2025-05-23 23:08:35 +0700

    feat: migrate App and Sidebar components to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css"
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
