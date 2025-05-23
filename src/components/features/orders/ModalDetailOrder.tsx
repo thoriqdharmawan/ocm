2eea4b94047e15386c929b30d3f7bc060cb22805
thoriqdharmawan <thoriqd21@gmail.com>
2025-05-23 17:40:31 +0700

    feat: integrate image download functionality in ModalDetailOrder component

--- src/components/features/orders/ModalDetailOrder.tsx
+++ src/components/features/orders/ModalDetailOrder.tsx
@@ -4,6 +4,7 @@ import { OrdersType } from "../../../models/orders";
 import { formatDate } from "../../../utils/global";
 import StatusBadge from "../../ui/StatusBadge";
 import Barcode from "react-barcode";
+import { useDownloadImage } from "../../../hooks/useDownloadImage";
 
 interface ModalDetailCustomerProps {
   open: boolean;
@@ -14,9 +15,11 @@ interface ModalDetailCustomerProps {
 const ModalDetailOrder = (props: ModalDetailCustomerProps) => {
   const { open, onClose, data } = props;
 
+  const { elementRef, downloadImage } = useDownloadImage();
+
   return (
     <Modal open={open} title="Detail Order" onClose={onClose}>
-      <div id="detail-order" className="bg-white p-3">
+      <div ref={elementRef} id="detail-order" className="bg-white p-3">
         <div className="row">
           <div className="col">
             <Field label="Order Number" value={data?.orderNumber || ""} />
@@ -59,6 +62,19 @@ const ModalDetailOrder = (props: ModalDetailCustomerProps) => {
             />
           </div>
         </div>
+      </div>
+
+      <div className="d-flex p-3 justify-content-start">
+        <button
+          onClick={() =>
+            downloadImage(`${data?.trackingNumber} - ${data?.customerName}`)
+          }
+          className="btn btn-sm btn-outline-primary"
+          type="button"
+          id="button-download-customer"
+        >
+          Download as Image
+        </button>
       </div>
     </Modal>
   );
